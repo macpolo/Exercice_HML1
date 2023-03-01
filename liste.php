19fe100fbcc3ad4a32134ae0284d77a88ef5c65e
macpolo <67027213+macpolo@users.noreply.github.com>
2023-03-01 11:59:50 +0100

    Create liste.php

---
+++ liste.php
@@ -0,0 +1,24 @@
+<html>
+<body>
+<h1>Liste des films</h1>
+
+
+<?php
+//Lister le contenu de la table movies
+
+//1° - Connexion à la BDD
+$base = new PDO('mysql:host=localhost; dbname=id20205709_movies', 'id20205709_macpolo', 'Daniela75/');
+$base->exec("SET CHARACTER SET utf8");
+
+//2° - Préparation de requette et execution
+$retour = $base->query('SELECT * FROM movies;');
+
+//3° - Lecture du resultat de la requette
+while ($data = $retour->fetch()){
+echo $data['id']." ".$data['titre']." ".$data['genre']." ".$data['annee']."</br>";
+}
+
+?>
+
+</body>
+</html>
