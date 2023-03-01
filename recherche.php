94ad49f386f5a7cfe7163758e27f51ace634d787
macpolo <67027213+macpolo@users.noreply.github.com>
2023-03-01 12:58:04 +0100

    Update recherche.php

--- recherche.php
+++ recherche.php
@@ -3,17 +3,38 @@ if(isset($_POST['submit'])){
 	$ANNEE = $_POST['annee'];
 
 	$base = new PDO('mysql:host=localhost; dbname=id20205709_movies', 'id20205709_macpolo', 'Daniela75015/');
-	$retour = $base->query('SELECT * FROM movies WHERE annee='.$ANNEE.';');
+<form method="post" action="">
+    <select name="choix_annee" id="choix_annee">
+        <option>Sélectionnez l'année</option>
+        <?php
+            $req_annee = $base->query('SELECT DISTINCT annee FROM movies ORDER BY annee ASC');
+            while($annee = $req_annee->fetch()) {
+        ?>
+            <option value="<?php echo $annee['annee']; ?>"><?php echo $annee['annee']; ?></option>
+        <?php
+            }
+        ?>
+    </select>
+    <input type="submit" value="Rechercher" name="rechercher" />
+</form>
 
-	if($retour->rowCount() > 0){
-		echo '<ul>';
-		while($ligne = $retour->fetch()){
-			echo '<li><h3>'.$ligne['titre'].'</h3></li>';
-		}
-		echo '</ul>';
-	}
-	else{
-		echo 'Aucun film trouvé pour l\'année '.$ANNEE.'.';
-	}
-}
+<?php
+    if(isset($_POST['rechercher'])) {
+        $annee = $_POST['choix_annee'];
+        $retour = $base->query('SELECT * FROM movies WHERE annee='.$annee.';');
+        if($retour->rowCount() > 0) {
+?>
+            <h3>La liste des films de l'année <?php echo $annee; ?></h3>
+            <ul>
+<?php
+            while($film = $retour->fetch()) {
+?>
+                <li><?php echo $film['titre']; ?></li>
+<?php
+            }
+?>
+            </ul>
+<?php
+        }
+    }
 ?>
